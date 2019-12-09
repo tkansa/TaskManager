0464df16bd0ffdbcbedc0bb217aba2bc54cbd480
Tiia Kansa <como2e2@gmail.com>
2019-12-09 14:45:36 -0500

    Added toggleCheckbox method to controller  to update DB and accordingly updated the Html and javascript to send the arguments

--- Controllers/TaskModelsController.cs
+++ Controllers/TaskModelsController.cs
@@ -129,6 +129,47 @@ namespace TaskManager.Controllers
             return View(taskModel);
         }
 
+        // Toggles the TaskCompleted checkbox
+        public async Task<IActionResult> ToggleTaskCompletedCheckbox(int? id, bool checkBoxStatus)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //Get the task from the db and update the done field
+            TaskModel taskModel = await _context.Task.FindAsync(id);
+            if (taskModel == null)
+            {
+                return NotFound();
+            }
+            taskModel.TaskCompleted = checkBoxStatus;
+
+            // Update the db
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(taskModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TaskModelExists(taskModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
         // GET: TaskModels/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggleTaskCompletedCheckbox(id) {
+    var checkBoxStatus = document.getElementById(id).checked;
+    
+    debugger;
+ 
+    var url = "/TaskModels/ToggleTaskCompletedCheckbox";
+    $.get(url, { id: id, checkBoxStatus: checkBoxStatus });
+
+
+}
